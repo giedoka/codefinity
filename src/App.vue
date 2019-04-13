d0bc0217204a5885cf611076bde02c5ecaf815e3
giedoka <pasiak5@gmail.com>
2019-04-14 00:34:08 +0200

    First commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="main-container">
+    <img alt="Codefinity" src="./assets/logo.svg">
+  </div>
+</template>
+
+<style lang="scss">
+.main-container {
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  img {
+    max-width: 100%;
+  }
+}
+</style>
